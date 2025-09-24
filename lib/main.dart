873f2d04622d86a258eec5509a772792ec5da35c
Aya Hammad <ayahammad3468@gmail.com>
2025-09-24 04:12:20 +0300

    Add assets and implement AccountSetup screen with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/ai.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,10 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-    );
+      debugShowCheckedModeBanner: false,
+      home:AccountSetup(),);
   }
-}   
+}
+
