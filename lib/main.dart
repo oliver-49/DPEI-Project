ca249838a7ba7021191d3b42421dd74175094c88
yousefsultan <yousefsultan280@gmail.com>
2025-10-15 23:24:02 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
-import 'package:dpei_project/presentation/screens/account_setup_serviceProvider/accountService.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:fixit/booking_services/cubit/booking_cubit.dart';
+import 'package:fixit/booking_services/view/providerprofile.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => BlocProvider(
+      create: (context) => BookingCubit(),
+      child: MyApp()), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home:AccountSetup(),);
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xfff9f9f9),
+      ),
+      home:  ProviderProfile(),
+    );
   }
 }
-
