e0bf6b4d68572489e0329c279fb5e98800c2ca60
ayahhamed <ayahammad3468@gmail.com>
2025-10-25 10:43:38 -0800

    firebase file

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dpei_project/presentation/screens/account_service/Selction_view/selection_cubit.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'core/services/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
